2c174f4929fa35e1e9429ca0a0b69618d7d9ee3e
Martin Cupak <cupak.martin90@gmail.com>
2020-10-19 06:43:12 +0100

    Add project files.

---
+++ CalculatorUI/CalculatorUI.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CalculatorUI
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CalculatorUI : ContentPage
+    {
+        public CalculatorUI()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
